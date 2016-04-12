f4c702f99270e9533e7901d99387dc4605a59459
Zachary Lester <zachary.greg.lester@gmail.com>
2016-04-12 12:49:42 -0700

    Make numbers in prompt more human readable

--- prompts/i-got-that-reference.js
+++ prompts/i-got-that-reference.js
@@ -1,11 +1,11 @@
-var num = 9001;
+var num = 1336;
 var name = 'Zach';
 var obj1 = {val: 'foo'};
 var obj2 = {val: 'bar'};
 var obj3 = {val: 'baz'};
 
 function alter(num, name, obj1, obj2) {
-  num += 1337;
+  num += 1;
   name = 'Beth';
   obj1 = obj3;
   obj2.val = 'qux';
