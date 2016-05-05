7e3002a3c659450c0a335ec7f3d6806be6ff8c20
Zachary Lester <zachary.greg.lester@gmail.com>
2016-05-04 19:08:12 -0700

    Add throttle prompt, move coding prompts to "practical" folder

---
+++ prompts/practical/full-throttle.js
@@ -0,0 +1,7 @@
+// Implement throttle
+
+// Throttle should accept a function and a duration as arguments 
+// and return a version of the function which will only be executed 
+// every duration ms
+
+var throttle = function() {};
