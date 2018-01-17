fbbf82fb54e8dbf9e13c0da0224aaa397541a754
Zachary Lester <zlester@rubiconproject.com>
2018-01-17 14:05:33 -0800

    Add non-euclidean-javascript

---
+++ prompts/non-euclidean-javascript.js
@@ -0,0 +1,16 @@
+function impossible () {
+    var a;
+
+    if (a == 1 && a == 2 && a == 3) {
+        console.log('foo');
+    } else {
+        console.log('bar')
+    }
+}
+
+impossible();
+
+// Without modifying the if statement, 
+// is it possible to make the above log 'bar'?
+
+// If so, how?
